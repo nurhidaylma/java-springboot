733bfdf7671465c24f65feb155f0d87eb74a81dd
nurhidaylma <ilmanurhidayati752@gmail.com>
2022-06-05 16:00:32 +0700

    Finish problem 3

---
+++ basic-java/src/Problem3.java
@@ -0,0 +1,21 @@
+// This program lists the factorial number of an input number
+import java.util.Scanner;
+
+public class Problem3 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Masukkan angka: ");
+    int number = input.nextInt();
+
+    findFactor(number);
+  }
+
+  public static void findFactor(int number) {
+    for (int i = 1; i <= number; i++) {
+      if (number%i == 0) {
+        System.out.println(i);
+      }
+    }
+  }
+}
